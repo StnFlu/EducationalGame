9ed0933d2a4b89dfc583130f7204d1e13b34f233
StnFlu <60700911+StnFlu@users.noreply.github.com>
2020-03-13 01:53:46 +0000

    1.2
    Menu is actaully looking nice

--- Assets/MainMenuNav.cs
+++ Assets/MainMenuNav.cs
@@ -17,68 +17,27 @@ public class MainMenuNav : MonoBehaviour
 
     public bool StartGame = true;
 
+    public GameObject camHolder;
+
+    public int option;
+
+    private Vector3 pointingTarget;
 
     // Start is called before the first frame update
     void Start()
     {
-        cam.transform.SetPositionAndRotation(menu.position, menu.rotation);
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (StartGame)
-        {
+      
 
 
-
-            RaycastHit hover;
-            Ray ray2 = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            if (Physics.Raycast(ray2, out hover))
-            {
-
-                if (hover.collider.name == "StartGame")
-                {
-
-
-                    cam.transform.SetPositionAndRotation(Vector3.Slerp(cam.transform.position, start.position, .1f), start.rotation);
-
-                    if (Input.GetKey(KeyCode.Mouse0))
-                    {
-                        test.SetActive(true);
-                        StartGame = false;
-                    }
-                    if (hover.collider.name == "ExitGame")
-                    {
-
-                        cam.transform.LookAt(hover.collider.transform.position);
-                        cam.transform.position = Vector3.Slerp(cam.transform.position, exit.position, .1f);
-                    }
-
-
-                }
-
-
-            }
-            else
-            {
-                cam.transform.position = Vector3.Slerp(cam.transform.position, menu.position, .1f);
-
-
-            }
-
-
-
-
-        }
-        else
-        {
-            cam.transform.position = Vector3.Slerp(cam.transform.position, game.position, .1f);
-            cam.transform.rotation = Quaternion.Slerp(cam.transform.rotation, game.rotation, .1f);
-        }
 
 
     }
+    
 }
---
+++ Assets/lookAtMouse.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lookAtMouse : MonoBehaviour
+{
+    public float sensitivity = 0.05f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Camera mycam = GetComponent<Camera>();
+
+        float mouseX = (Input.mousePosition.x / Screen.width) - 0.5f;
+        float mouseY = (Input.mousePosition.y / Screen.height) - 0.5f;
+        mycam.transform.localRotation = Quaternion.Euler(new Vector4(-1f * (mouseY * 180f), mouseX * 360f, transform.localRotation.z));
+
+    }
+}
