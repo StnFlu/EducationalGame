7ae7f11d081b14f83ac504d46c28fd0e0a57df1b
NeoXypher <Aquiliawtf@gmail.com>
2020-04-18 03:09:35 +0100

    Pause Menu and Settings
    Added more UI elements. Need to be coded with the corresponding scripts. Elements can not be currently interacted with. Unsure why.

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    void PauseGame()
+    {
+        Time.timescale = 0f;
+    }
+
+    void UnPauseGame()
+    {
+        Time.timescale = 1f;
+    }
+
+    void ExitGame()
+    {
+
+    }
+
+    void AdjustEffects()
+    {
+
+    }
+
+    void AdjustMusic()
+    {
+
+    }
+
+    void Fullscreen()
+    {
+
+    }
+
+    void Borderless()
+    {
+
+    }
+
+    void Windowed()
+    {
+
+    }
+}
